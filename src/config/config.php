1cc85fda962158d7ebcac356592095175b59299b
Danijel Dedic <dedic.c@gmail.com>
2013-11-03 23:35:39 +0100

    config file

---
+++ src/config/config.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | API header response.
+    |--------------------------------------------------------------------------
+    |
+    | Default API will always return "200", but you can set the right header
+    | returned by change "true".
+    |
+    */
+
+    'gateway.enabled'           => false,
+    'gateway'                   => 'Nexmo\NexmoGateway',
+    'gateway.api_key'           => '',
+    'gateway.api_secret'        => '',
+
+);
+
+
